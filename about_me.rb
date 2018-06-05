5de381eb711748fd15b817e56ee740d2d53a6032
Kai-Cai <kaiaschiff@gmail.com>
2018-06-05 06:08:50 +0000

    Done.

---
+++ about_me.rb
@@ -0,0 +1,11 @@
+def about_me
+  puts "I am from China."
+  puts "I was adopted when I was a year old."
+  puts "I am currently 15 years old and going into my Sophomore year at Cherry Creek High School"
+  puts "I love learning about math, science, and art."
+  puts "I play the guitar, do theater (cast and crew), and skateboard."
+  puts "I would love to meet you! Come say 'Hi!' to me."
+end
+
+about_me
+
